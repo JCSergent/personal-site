689f84cccb38050a039a6452656e14f5fd8bf9f0
JC Sergent <jcsergent20@gmail.com>
2024-12-02 14:28:30 -0500

    style dump

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 var scale = 100
 
-function stokeFlame() {
-    scale += 10
-    document.getElementById("fire-group").style.scale = scale + '%'
+// function stokeFlame() {
+//     scale += 10
+//     document.getElementById("fire-group").style.scale = scale + '%'
 
-}
+// }
 
-document.getElementById("fire-group").addEventListener("click", stokeFlame);
+// document.getElementById("fire-group").addEventListener("click", stokeFlame);
