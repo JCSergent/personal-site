116a105ccef67643b026a5dcfba4c7c2f0886e47
JC Sergent <jcsergent20@gmail.com>
2024-12-03 11:37:00 -0500

    redo!

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 
 var scale = 100
 
-// function stokeFlame() {
-//     scale += 10
-//     document.getElementById("fire-group").style.scale = scale + '%'
+function matchClick(event) {
+    // console.log(event)
+    // console.log(event.srcElement)
+    const pos = event.target.id;
 
-// }
+    const left = (parseInt(pos[0]) - 1).toString() + pos[1]
+    const right = (parseInt(pos[0]) + 1).toString() + pos[1]
+    const up = pos[0] + (parseInt(pos[1]).toString() - 1)
+    const down = pos[0] + (parseInt(pos[1]) + 1).toString()
 
-// document.getElementById("fire-group").addEventListener("click", stokeFlame);
+    console.log([left, right, up, down])
+
+
+    for (const match of [left, right, up, down]) {
+        if (document.getElementById(match)) {
+            console.log(document.getElementById(match).innerHTML)
+            if (document.getElementById(match).innerHTML === 'O') {
+                document.getElementById(match).innerHTML = 'X'
+            } else {
+                document.getElementById(match).innerHTML = 'O'
+            }
+        }
+    }
+}
+
+for (const elem of document.getElementsByClassName("match-icon")) {
+    elem.addEventListener("click", matchClick);
+}
