723e8a80a0d380332ecec3c6c8400fa381c62163
JC Sergent <jcsergent20@gmail.com>
2024-12-04 15:10:05 -0500

    memory!

--- script.js
+++ script.js
@@ -38,12 +38,13 @@ function matchClick(event) {
     const down = pos[0] + (parseInt(pos[1]) + 1).toString()
 
     for (const match of [pos, left, right, up, down]) {
-        if (document.getElementById(match)) {
-            console.log(document.getElementById(match).innerHTML)
-            if (document.getElementById(match).innerHTML === 'O') {
-                document.getElementById(match).innerHTML = 'X'
+        const element = document.getElementsByClassName("match-icon").namedItem(match);
+
+        if (element) {
+            if (element.innerHTML === 'O') {
+                element.innerHTML = 'X'
             } else {
-                document.getElementById(match).innerHTML = 'O'
+                element.innerHTML = 'O'
             }
         }
     }
@@ -104,7 +105,7 @@ function glyphClick(event) {
 function shiftGlyph(pos, shift) {
     pos = clampGlyph(pos);
 
-    const glyphElement = document.getElementById(pos.toString());
+    const glyphElement = document.getElementsByClassName("glyph-icon").namedItem(pos.toString());
     const glyph = glyphElement.innerHTML;
     const newGlyph = GLYPHS.findIndex((g) => g === glyph);
     const i = clampGlyph(newGlyph + shift);
@@ -132,4 +133,138 @@ resetGylph();
 
 for (const elem of document.getElementsByClassName("glyph-icon")) {
     elem.addEventListener("click", glyphClick);
+}
+
+// MEMORY GAME
+
+let isAwaitingInput = false;
+let highScore = 0;
+let score = 0;
+let currIndex = 0;
+let sequence = [];
+const startMemoryButton = '<div class="game-actions" id="start-memory"><button tooltip="Start" onclick="playMemoryTurn()">Start</button></div>'
+const centerGrid = document.getElementsByClassName("memory-icon").namedItem('11');
+
+async function memoryClick(event) {
+    if (!isAwaitingInput) {
+        return;
+    }
+    isAwaitingInput = false;
+    for (const elem of document.getElementsByClassName("memory-icon")) {
+        elem.classList.remove('styled-hover');
+    }
+    if (event.target.id === sequence[currIndex]) { //correct
+        currIndex += 1;
+        event.target.innerHTML = '!';
+        await new Promise(resolve => setTimeout(resolve, 800));
+        event.target.innerHTML = '&nbsp;';
+        if (currIndex > sequence.length - 1) {
+            console.log('finish turn')
+            score += 1;
+            document.getElementById('memory-score').innerHTML = score;
+            document.getElementsByClassName("memory-icon").namedItem('00').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('01').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('02').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('10').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('11').innerHTML = 'NICE';
+            document.getElementsByClassName("memory-icon").namedItem('12').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('20').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('21').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('22').innerHTML = '!';
+            await new Promise(resolve => setTimeout(resolve, 800));
+            for (const elem of document.getElementsByClassName("memory-icon")) {
+                elem.innerHTML = '&nbsp;';
+            }
+            await new Promise(resolve => setTimeout(resolve, 800));
+            playMemoryTurn();
+
+        } else {
+            for (const elem of document.getElementsByClassName("memory-icon")) {
+                elem.classList.add('styled-hover');
+            }
+            isAwaitingInput = true;
+        }
+
+    } else { //incorrect
+        for (const elem of document.getElementsByClassName("memory-icon")) {
+            elem.innerHTML = 'X';
+        }
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        document.getElementsByClassName("memory-icon").namedItem('00').innerHTML = '!';
+        document.getElementsByClassName("memory-icon").namedItem('01').innerHTML = 'FINAL';
+        document.getElementsByClassName("memory-icon").namedItem('02').innerHTML = '!';
+        document.getElementsByClassName("memory-icon").namedItem('10').innerHTML = '!';
+        document.getElementsByClassName("memory-icon").namedItem('11').innerHTML = 'SCORE';
+        document.getElementsByClassName("memory-icon").namedItem('12').innerHTML = '!';
+        document.getElementsByClassName("memory-icon").namedItem('20').innerHTML = '!';
+        document.getElementsByClassName("memory-icon").namedItem('21').innerHTML = score;
+        document.getElementsByClassName("memory-icon").namedItem('22').innerHTML = '!';
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        if (score > highScore) {
+            document.getElementsByClassName("memory-icon").namedItem('00').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('01').innerHTML = 'NEW';
+            document.getElementsByClassName("memory-icon").namedItem('02').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('10').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('11').innerHTML = 'HIGH';
+            document.getElementsByClassName("memory-icon").namedItem('12').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('20').innerHTML = '!';
+            document.getElementsByClassName("memory-icon").namedItem('21').innerHTML = 'SCORE';
+            document.getElementsByClassName("memory-icon").namedItem('22').innerHTML = '!';
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            highScore = score;
+            document.getElementById('memory-highscore').innerHTML = highScore;
+        }
+
+        resetMemoryGame();
+    }
+}
+
+async function playMemoryTurn() {
+    const newPos = Math.floor(Math.random() * 2).toString() + Math.floor(Math.random() * 2).toString()
+    sequence.push(newPos);
+
+    centerGrid.innerHTML = '3';
+    await new Promise(resolve => setTimeout(resolve, 800));
+    centerGrid.innerHTML = '2';
+    await new Promise(resolve => setTimeout(resolve, 800));
+    centerGrid.innerHTML = '1';
+    await new Promise(resolve => setTimeout(resolve, 800));
+    centerGrid.innerHTML = '&nbsp;';
+    await new Promise(resolve => setTimeout(resolve, 800));
+
+    for (const pos of sequence) {
+        const element = document.getElementsByClassName("memory-icon").namedItem(pos);
+        element.innerHTML = '!';
+        await new Promise(resolve => setTimeout(resolve, 800));
+        element.innerHTML = '&nbsp;';
+        await new Promise(resolve => setTimeout(resolve, 500)); // half second
+    }
+    
+    for (const elem of document.getElementsByClassName("memory-icon")) {
+        elem.classList.add('styled-hover');
+    }
+    currIndex = 0;
+    isAwaitingInput = true;
+}
+
+
+function resetMemoryGame() {
+    for (const elem of document.getElementsByClassName("memory-icon")) {
+        elem.innerHTML = '&nbsp;';
+        elem.classList.remove('styled-hover');
+    }
+    
+    sequence = [];
+    currIndex = 0;
+    score = 0;
+    document.getElementById('memory-score').innerHTML = score;
+    isAwaitingInput = false;
+    centerGrid.style.opacity = '100%';
+    centerGrid.innerHTML = startMemoryButton;
+}
+
+resetMemoryGame();
+
+for (const elem of document.getElementsByClassName("memory-icon")) {
+    elem.addEventListener("click", memoryClick);
 }
